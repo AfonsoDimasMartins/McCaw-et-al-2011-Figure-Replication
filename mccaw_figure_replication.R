90a410ae17ccc34afa9abffe25bf718b230f813f
Afonso Dimas Martins <afonsodimasmartins@gmail.com>
2021-02-26 10:36:00 +0100

    Cleaned up some redundant bits.

--- mccaw_figure_replication.R
+++ mccaw_figure_replication.R
@@ -1,23 +1,21 @@
 # Proportion of the mutant strain in the donor and in the recipient
-# Data from McCaw et al (2011)
-# https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1002026
 mut_donor <- c(0, 8, 9, 12, 34.5, 60, 82, 95, 99) / 100
 mut_recip <- c(0, 0, 20, 43, 68, 33, 63, 99, 94) / 100
 
 # Function to obtain model curves (for fitting or plotting)
-# Equation also from McCaw et al (2011)
+# Input should be one number
 model_curve <- function(s) {
   curve <- mut_donor / (mut_donor + (1 - mut_donor) * exp(s))
   return(curve)
 }
 
 # Function that gives the mean squared errors for a certain s value
+# Input should be a vector of length 9 to match the data
 model_mse <- function(starting_curve) {
   error <- starting_curve - mut_recip
   mse <- mean(error)^2
   return(mse)
 }
-
 
 starting_curve <- model_curve(-0.5)
 model_fit <- optim(par = starting_curve, fn = model_mse,
