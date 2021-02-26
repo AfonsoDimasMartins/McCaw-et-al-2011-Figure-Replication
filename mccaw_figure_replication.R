3581895105def44274b23f00fc412ab991352673
Afonso Dimas Martins <afonsodimasmartins@gmail.com>
2021-02-26 08:37:57 +0100

    Removed unnecessary functions since R is more efficient with vectorized code and simplified redundant parts.

--- mccaw_figure_replication.R
+++ mccaw_figure_replication.R
@@ -4,95 +4,61 @@
 mut_donor <- c(0, 8, 9, 12, 34.5, 60, 82, 95, 99) / 100
 mut_recip <- c(0, 0, 20, 43, 68, 33, 63, 99, 94) / 100
 
-# Function for model fitting
+# Function to obtain model curves (for fitting or plotting)
 # Equation also from McCaw et al (2011)
-mean_squared_errors <- function(s) {
-  model_attempt <- c(); error <- c()
-  for(i in 1:9) {
-    p <- mut_donor[i]
-    model_attempt <- c(model_attempt, p / (p + (1 - p) * exp(s)))
-    delta <- model_attempt[i] - mut_recip[i]
-    error <- c(error, delta)
-  }
-  mse <- mean(error^2)
+model_curve <- function(s) {
+  curve <- mut_donor / (mut_donor + (1 - mut_donor) * exp(s))
+  return(curve)
+}
+
+# Function that gives the mean squared errors for a certain s value
+model_mse <- function(starting_curve) {
+  error <- starting_curve - mut_recip
+  mse <- mean(error)^2
   return(mse)
 }
 
-theta.guess <- -0.5
-model_fit <- optim(par = theta.guess, fn = mean_squared_errors,
+
+starting_curve <- model_curve(-0.5)
+model_fit <- optim(par = starting_curve, fn = model_mse,
                    method = "L-BFGS-B", hessian = T)
 
 
-# Figure 4A reproduction
-# Plotting the best model fitting plus interval curves provided in the article
-plot(NA, NA, xlim = c(0, 1.01), ylim = c(0, 1.01),
-     xlab = "Proportion of mutant in donor",
-     ylab = "Proportion of mutant in recipient",
-     xaxs = "i", yaxs = "i", fg = "white", col.axis = "white")
-axis(1); axis(2, las = 1)
-best_curve <- c()
-s <- model_fit$par
-for (i in 1:9) {
-  p <- mut_donor[i]
-  best_curve <- c(best_curve, p / (p + (1 - p) * exp(s)))
-}
-lines(mut_donor, best_curve, col = "coral3", lwd = 2)
-P <- c()
-s <- -log(0.44)
-for (i in 1:100) {
-  p <- seq(0, 1, length.out = 100)[i]
-  P <- c(P, p / (p + (1 - p) * exp(s)))
-}
-lines(seq(0, 1, length.out = 100), P, lwd = 2, lty = 2)
-P <- c()
-s <- -log(3.9)
-for (i in 1:100) {
-  p <- seq(0, 1, length.out = 100)[i]
-  P <- c(P, p / (p + (1 - p) * exp(s)))
-}
-lines(seq(0, 1, length.out = 100), P, lwd = 2, lty = 2)
-points(mut_donor, mut_recip, pch = 0, lwd = 1.5)
-
+# Plotting elements
+x_lab <- "Proportion of mutant in donor"
+y_lab <- "Proportion of mutant in recipient"
 
 
 # Figure 1 and 2 reproduction
 op <- par(mfrow = c(1, 2))
-plot(NA, NA, xlim = c(0, 1.01), ylim = c(0, 1.01),
-     xlab = "Proportion of mutant in donor",
-     ylab = "Proportion of mutant in recipient",
+
+plot(NA, NA, xlim = c(0, 1.01), ylim = c(0, 1.01), xlab = x_lab, ylab = y_lab,
      xaxs = "i", yaxs = "i", fg = "white", col.axis = "white")
 points(mut_donor, mut_recip, pch = 0)
 axis(1); axis(2, las = 1)
 text(0.1, 0.9, "a)")
-plot(NA, NA, xlim = c(0, 1.01), ylim = c(0, 1.01),
-     xlab = "Proportion of mutant in donor",
-     ylab = "Proportion of mutant in recipient",
+
+plot(NA, NA, xlim = c(0, 1.01), ylim = c(0, 1.01), xlab = x_lab, ylab = y_lab,
      xaxs = "i", yaxs = "i", fg = "white", col.axis = "white")
-P <- c()
-s <- -1.5
-for (i in 1:9) {
-  p <- mut_donor[i]
-  P <- c(P, p / (p + (1 - p) * exp(s)))
-}
-lines(mut_donor, P, col = "gray80", lwd = 2, lty = 1)
-P <- c()
-s <- 0
-for (i in 1:9) {
-  p <- mut_donor[i]
-  P <- c(P, p / (p + (1 - p) * exp(s)))
-}
-lines(mut_donor, P, col = "gray60", lwd = 2, lty = 2)
-P <- c()
-s <- 1.5
-for (i in 1:9) {
-  p <- mut_donor[i]
-  P <- c(P, p / (p + (1 - p) * exp(s)))
-}
-lines(mut_donor, P, col = "gray40", lwd = 2, lty = 3)
+lines(mut_donor, model_curve(-1.5), col = "gray80", lwd = 2, lty = 1)
+lines(mut_donor, model_curve(0), col = "gray60", lwd = 2, lty = 2)
+lines(mut_donor, model_curve(1.5), col = "gray40", lwd = 2, lty = 3)
 legend("bottomright", legend = c("s = -1.5", "s = 0", "s = 1.5"),
        lty = 1:3, lwd = 2,
        col = c("gray80", "gray60", "gray40"),
        cex = 1, box.lty = 0, bty = "n", seg.len = 3)
 axis(1); axis(2, las = 1)
 text(0.1, 0.9, "b)")
+
 par(op)
+
+
+# Figure 4A reproduction
+# Plotting the best model fitting plus interval curves provided in the article
+plot(NA, NA, xlim = c(0, 1.01), ylim = c(0, 1.01), xlab = x_lab, ylab = y_lab,
+     xaxs = "i", yaxs = "i", fg = "white", col.axis = "white")
+axis(1); axis(2, las = 1)
+lines(mut_donor, model_fit$par, col = "coral3", lwd = 2)
+lines(mut_donor, model_curve(-log(0.44)), lwd = 2, lty = 2)
+lines(mut_donor, model_curve(-log(3.9)), lwd = 2, lty = 2)
+points(mut_donor, mut_recip, pch = 0, lwd = 1.5)
